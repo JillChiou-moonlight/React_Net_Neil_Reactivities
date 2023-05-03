4e6c02b78ff2c670062c4f036b08df406cf4a01d
jillchiou <jillchiou.job@gmail.com>
2023-05-03 20:25:44 +0800

    End of section 2

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // localhost:5000/
+
+    public class BaseApiController:ControllerBase
+    {
+        
+    }
+}
